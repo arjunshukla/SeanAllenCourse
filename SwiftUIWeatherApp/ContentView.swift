b9648687a357bb929e592fc87e8b99b6162766a5
Arjun Shukla <arjun.shukla@outlook.com>
2022-09-02 14:53:28 -0700

    Initial Commit

---
+++ SwiftUIWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Arjun Shukla on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWeatherApp/SwiftUIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppApp.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Arjun Shukla on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
